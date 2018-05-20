f413092f5a5115f5610de5afad771c7025d3682e
Giancarlo Buenaflor <giancarlo.buenaflor@yahoo.com>
2018-05-20 14:09:29 +0200

    Initial commit

---
+++ src/components/Register/Register.js
@@ -0,0 +1,105 @@
+import React, { Component } from 'react';
+
+class Register extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            registerName: '',
+            registerEmail: '',
+            registerPassword: ''
+        }
+    }
+    
+    onNameChange = (e) => {
+        this.setState({ registerName: e.target.value });
+    }
+
+
+    onEmailChange = (e) => {
+        this.setState({ registerEmail: e.target.value });
+    }
+
+    onPasswordChange = (e) => {
+        this.setState({ registerPassword: e.target.value });
+    }
+
+    onSubmitRegister = (e) => {
+        const { registerEmail, registerPassword, registerName } = this.state;
+        console.log(registerEmail, registerPassword, registerName);
+        fetch('http://localhost:3000/register', {
+            method: 'post',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({
+                email: registerEmail,
+                password: registerPassword,
+                name: registerName
+            })
+        })
+            .then(response => response.json())
+            .then(user => {
+                if (user) {
+                    this.props.loadUser(user)
+                    this.props.onRouteChange('home');
+                } else {
+                    alert('Error has occured');
+                }
+            })   
+
+        // this.props.onRouteChange('home');
+    }
+    render() {
+        return (
+            <article className="br2 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 center">
+                <main className="pa4 black-80">
+                    <div className="measure">
+                        <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
+                            <legend className="f1 fw6 ph0 mh0">Register</legend>
+                            <div className="mt3">
+                                <label className="db fw6 lh-copy f6" htmlFor="name">Name</label>
+                                <input
+                                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                                    type="text"
+                                    name="name"
+                                    id="name"
+                                    onChange={this.onNameChange}
+                                />
+                            </div>
+                            <div className="mt3">
+                                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                                <input
+                                    className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                                    type="email"
+                                    name="email-address"
+                                    id="email-address"
+                                    onChange={this.onEmailChange}
+                                />
+                            </div>
+                            <div className="mv3">
+                                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                                <input
+                                    className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                                    type="password"
+                                    name="password"
+                                    id="password"
+                                    onChange={this.onPasswordChange}
+                                />
+                            </div>
+                        </fieldset>
+                        <div className="">
+                            <input
+                                onClick={this.onSubmitRegister}
+                                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                                type="submit"
+                                value="Register"
+                            />
+                        </div>
+                    </div>
+                </main>
+            </article>
+        );
+    }
+}
+
+
+export default Register;
